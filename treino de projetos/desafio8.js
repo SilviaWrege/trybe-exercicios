643308a3ce0070d7925d31b40b40c6a824f928b1
Silvia-Wrege <silviawrege@gmail.com>
2023-02-03 15:32:56 +0000

    desafio8

--- treino de projetos/desafio8.js
+++ treino de projetos/desafio8.js
@@ -3,13 +3,13 @@ meuArray = [2, 15, 7, 9, 45];
 
 function fizzBuzz (parametro){
   let novoArray = [];
-    for(index = 0; index <= parametro.length; index += 1){
-        if(meuArray[index] % 3 == 0){
-        novoArray.push("fizz");
-    } else if (meuArray[index] % 5 == 0){
+    for(index = 0; index < parametro.length; index += 1){
+        if ( parametro[index] % 3 == 0 && parametro[index] % 5 == 0){
+        novoArray.push("fizzBuzz");
+    } else if (parametro[index] % 5 == 0){
         novoArray.push("buzz");
-    } else if ( meuArray[index] % 3 == 0 && meuArray[index] % 5 == 0){
-       novoArray.push("fizzBuzz");
+    } else if (parametro[index] % 3 == 0){
+       novoArray.push("fizz");
     } else { 
        novoArray.push("bug!");
     }
