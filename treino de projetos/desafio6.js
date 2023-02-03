b47aa3a75aa96a0ac41367438abf1a812481e2eb
Silvia-Wrege <silviawrege@gmail.com>
2023-02-03 13:39:52 +0000

    desafio7

--- treino de projetos/desafio6.js
+++ treino de projetos/desafio6.js
@@ -1,18 +1,29 @@
-function calcTriangleArea(base,height) {
+function calcTriangleArea(base, height) {
     let area = (base * height) / 2
     return area;
 }
 
-//console.log(calcTriangleArea(5,8))
-
-function calcRectangleArea(base,height) {
+function calcRectangleArea(base, height) {
     let area = (base * height);
     return area;
 }
 
-function calcAllAreas(base,height,form) {
-    
-    let area = calcTriangleArea;
-    return area;
+function calcAllAreas(base, height, form) {
+    let area = 0;
+    let mensagem = '';
+    if (form === 'triângulo') {
+        area = calcTriangleArea(base, height);
+        mensagem = `O valor da área do triângulo é de: ${area}`;
+
+    } else if (form === 'retângulo') {
+        area = calcRectangleArea(base, height);
+        mensagem = `O valor da área do retângulo é de: ${area}`;
+
+    } else {
+        mensagem = 'Não foi possível fazer o cálculo, insira uma forma geométrica válida';
+
+    }
+    return mensagem;
 }
 
+console.log(calcAllAreas(10,50, "triângulo"));
