1890a402161ad4c063feddb4a36b75505025c0d3
Silvia Wrege <silviawrege@gmail.com>
2023-02-09 13:58:57 +0000

    salvando modificações de pastas

---  secao-05-HTML e CSS Forms, Flexbox e Responsivo/secao-5-dia-01/script.js
+++  secao-05-HTML e CSS Forms, Flexbox e Responsivo/secao-5-dia-01/script.js
@@ -1,8 +1,11 @@
 // SELETORES
 const INPUT_TEXT = document.querySelector("#input-text");
 
-
 const INPUT_CHECKBOX = document.querySelector("#input-checkbox");
 
-
 const HREF_LINK = document.querySelector("#href");
+
+
+function HREF_LINK( ) {
+
+}
